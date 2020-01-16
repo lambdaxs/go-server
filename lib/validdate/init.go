eed4dec225f44b3392cf66a6e2e543327b10f2ea
xiaoshen <753623217@qq.com>
2020-01-16 11:01:29 +0800

    新增快捷方法

--- lib/validdate/init.go
+++ lib/validdate/init.go
@@ -1,6 +1,7 @@
 package validdate
 
 import (
+    "errors"
     "fmt"
     "github.com/go-playground/locales/zh"
     ut "github.com/go-playground/universal-translator"
@@ -8,10 +9,14 @@ import (
     zh_translations "github.com/go-playground/validator/v10/translations/zh"
 )
 
-var GlobalValidate *validator.Validate
+var (
+    GlobalValidate *validator.Validate
+    Trans ut.Translator
+)
 
 func init() {
     GlobalValidate = New()
+
 }
 
 func New() *validator.Validate {
@@ -23,6 +28,20 @@ func New() *validator.Validate {
     trans, _ := uni.GetTranslator("zh")
     if err := zh_translations.RegisterDefaultTranslations(validate, trans);err != nil {
         fmt.Println("register validate error:"+err.Error())
+    }else {
+        Trans = trans
     }
     return validate
+}
+
+func Struct(i interface{}) error {
+    if GlobalValidate == nil {
+        return errors.New("GlobalValidate need init")
+    }
+    if err := GlobalValidate.Struct(i);err != nil {
+        for _,paramErr := range err.(validator.ValidationErrors) {
+            return errors.New(paramErr.Translate(Trans))
+        }
+    }
+    return nil
 }
