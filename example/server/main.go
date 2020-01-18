da26d6a25573939793457ba42af0ae01f344c7f0
xiaoshen <753623217@qq.com>
2020-01-18 14:58:26 +0800

    fix example

--- example/server/main.go
+++ example/server/main.go
@@ -4,11 +4,11 @@ import (
     "context"
     "flag"
     "fmt"
-    "github.com/go-playground/validator/v10"
     "github.com/labstack/echo"
     "github.com/lambdaxs/go-server/confu"
     hello "github.com/lambdaxs/go-server/example/discover/pb"
     "github.com/lambdaxs/go-server/server"
+    "github.com/lambdaxs/go-server/lib/validate"
 )
 
 type SayHelloServer struct {
@@ -26,8 +26,6 @@ func main() {
     confu.ParseFlag()
     flag.Parse()
 
-    vaildate := validator.New()
-
     httpSrv := server.HttpServer{
         Host: "127.0.0.1",
         Port: 9000,
@@ -38,15 +36,14 @@ func main() {
                 Uid   int64  `json:"uid" form:"uid" validate:"required"`
                 Age   int64  `json:"age" form:"age" validate:"required,gte=0,lte=130"`
                 Email string `json:"email" validate:"required,email"`
+                Code string `json:"code" validate:"required,len=4"`
+                Plat string `json:"plat" validate:"required,oneof=ios android"`
             })
             if err := c.Bind(reqModel); err != nil {
                 return c.JSON(200, err.Error())
             }
-            fmt.Println(reqModel)
-            if err := vaildate.Struct(reqModel); err != nil {
-                for _, paramErr := range err.(validator.ValidationErrors) {
-                    return c.JSON(200, "参数错误:"+paramErr.Field())
-                }
+            if err := validate.Struct(reqModel);err != nil {
+                return c.JSON(200, err.Error())
             }
             return c.JSON(200, "success")
         })
